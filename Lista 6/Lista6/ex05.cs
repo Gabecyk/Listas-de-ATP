ae0aef6c3ff462c84de13d323686842c0ae6f6ec
Gabriel Balduino <gabrielbalduino29@gmail.com>
2024-06-20 01:02:02 -0300

    Lista 6

--- Lista 6/Lista6/ex05.cs
+++ Lista 6/Lista6/ex05.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Linq.Expressions;
 using System.Text;
 
 class ex05
@@ -10,47 +9,61 @@ class ex05
         Console.Clear();
         Console.WriteLine("ex05");
 
-        int n = Numeros();
-        
-        bool tipo = Tipo(n);
-        if(n != 0)
-        {
-            recebeLinhas(n, tipo);
+        int n = Numeros(); //n recebe quantidade de linhas que serão preenchidas
+               
+        StreamWriter sw = new StreamWriter("D:\\ex05.txt", false, Encoding.ASCII);//Abre arquivo
 
-                        
+        if(n > 0) //O programa continua caso n não for maior que 0
+        {
+            recebeLinhas(n, sw); //user escreve as linhas
+            sw.Close(); //fecha arquivo
+
+            StreamReader sr = new StreamReader("D:\\ex05.txt");//abre arquivo para ler
+            
+            int cont = ExibirArquivo(sr); //Aqui exibi as linhas e retorna junto a qtd de linhas
+            sr.Close();//fecha arquivo
+
+            Console.WriteLine($"Este arquivo possui {cont} linhas!"); //qtd de linhas                         
         }
         else
-            Console.WriteLine("Por ser zero nada será feito!");
-        
-        
+            Console.WriteLine("Por ser menor que 1 nada será feito! \nTente com algum número maior.");        
     }
 
-    static int Numeros()
+    static int Numeros() //Salva quantas linha serão preenchidas
     {
         int n = 0;
         Console.WriteLine("Quantas linhas deseja colocar no arquivo?");
         n = int.Parse(Console.ReadLine());
-        return n;
-    }
-    
-    static bool Tipo(int n)
-    {
-        bool tipo;
-        if (n > 1)
-            tipo = true;
-            else 
-            tipo = false;
-
-        return tipo;
+        return n; //retorna com o valor da quatidade
     }
 
-    static void recebeLinhas(int n, bool tipo) 
+    static void recebeLinhas(int n, StreamWriter sw) //Preenche as linhas em base a qtd
     {
-        StreamWriter sw = new StreamWriter("ex05.txt", tipo, Encoding.ASCII);
         for (int i = 1; i < (n+1); i++)
         {
             Console.WriteLine($"Escreva a linha {i}:");
-            sw.Write  (Console.ReadLine());
+            sw.WriteLine (Console.ReadLine());
         }
+    }
+
+    static int ExibirArquivo(StreamReader sr) //Exibi as linhas do arquivo
+    {
+        int n = 0;
+        string exibir = sr.ReadLine(); //string recebe a primeira linha do arquivo
+
+        Console.WriteLine("Aqui está oque foi escrito no arquivo:");
+
+        //While até que seja null a string exibir
+        while (exibir != null) //a string será null no momento em que acabar de ler a ultima linha e receber de novo o 'sr', que dessa vez por ter acabado, voltará como null
+        {
+            n++; // soma qtd de linhas lidas
+            for(int i = 0; i < 1; i++)
+            {
+                Console.Write($"Linha {n}: {exibir}"); //exibe a linha do arquivo
+            }
+            Console.WriteLine();//Quebra a linha
+            exibir = sr.ReadLine(); //Recebe a outra linha do arquivo
+        }
+        return n; //retorna a qtd de linhas lidas
     }
 }
--- Lista 6/Lista6/obj/Debug/net8.0/Lista6.AssemblyInfo.cs
+++ Lista 6/Lista6/obj/Debug/net8.0/Lista6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lista6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bc67de7b8ebc97d6408605194b893e63e2029fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7cd78b32ae65034c7343cfc02e7e46c92a9b91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lista6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lista6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
